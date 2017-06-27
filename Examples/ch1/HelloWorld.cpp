9de6b2da62e37b70997c50e6a6578ad9698131d0
KumarGauravMishra <kumar.mishra@firstcry.com>
2017-06-27 15:12:32 +0530

    Initial Commit

---
+++ Examples/ch1/HelloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+* File: HelloWorld.cpp
+* =====================
+* This program is taken from c++ programming book by Denis Rechi.
+* This take the learner get passed the basic herdel of the
+* Learning new programming language.
+*/
+
+#include <iostream>
+using namespace std;
+
+/* Main program */
+
+int main() {
+  cout << "Hello World" << endl;
+  return 0;
+}
---
+++ Examples/ch1/PowerOfTwo.cpp
@@ -0,0 +1,41 @@
+/*
+* File: PowerOfTwo.cpp
+* =====================
+* This program generates a list to power of 2
+* upto an exponent limit enterd by the user.
+*/
+
+#include <iostream>
+using namespace std;
+
+/* Function Prototypes */
+int raiseToPower(int n, int k);
+
+/* Main Program */
+
+int main(){
+    int limit;
+    cout << "This program prints a lists power of 2" << endl;
+    cout << "Please enter a exponent : ";
+    cin >> limit;
+
+    for(int i = 0; i < limit; i++){
+        cout << "2 of " << i << " is : " << raiseToPower(2, i) << endl;
+    }
+    return 0;
+}
+
+/*
+* Function : raiseToPower
+* Usage : int p = raiseToPower(int n, int k);
+* ============================================
+* This function raise n to the kth power and return the result;
+*/
+
+int raiseToPower(int n, int k){
+    int result = 1;
+    for(int i = 0; i < k; i++){
+        result *= n;
+    }
+    return result;
+}
